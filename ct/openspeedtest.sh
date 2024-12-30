c19e40d7a7e7c0449bb6e4f7a7e489f3e1589f74
Gary Deeble <gdeeble@gmail.com>
2024-12-30 16:00:11 -0500

    Add scripts for Openspeedtest-Server

---
+++ ct/openspeedtest.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2024 Gary Deeble
+# Author: Gary Deeble (gdeeble)
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/openspeedtest/Speed-Test
+# Source: https://github.com/openspeedtest/Nginx-Configuration/
+
+# App Default Values
+APP="OpenSpeedtest-Server"
+var_tags="speedtest"
+var_cpu="2"
+var_ram="1024"
+var_disk="6"
+var_os="debian"
+var_version="12"
+var_unprivileged="1"
+
+# App Output & Base Settings
+header_info "$APP"
+base_settings
+
+# Core
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -d /opt/openspeedtest ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  msg_info "Stopping ${APP}"
+  systemctl stop nginx
+  msg_ok "Stopped ${APP}"
+
+  msg_info "Updating ${APP}"
+  cd /opt/openspeedtest
+  output=$(git pull --no-rebase)
+  if echo "$output" | grep -q "Already up to date."; then
+    msg_ok "$APP is already up to date."
+    exit
+  fi
+  msg_ok "Updated Successfully"
+
+  msg_info "Starting ${APP}"
+  systemctl start nginx
+  msg_ok "Started ${APP}"
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8081${CL}"
---
+++ install/openspeedtest-install.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 Gary Deeble
+# Author: Gary Deeble (gdeeble)
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/openspeedtest/Speed-Test
+# Source: https://github.com/openspeedtest/Nginx-Configuration/
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get update
+$STD apt-get install -y \
+  gpg \
+  curl \
+  sudo \
+  mc \
+  git \
+  mediainfo \
+  unrar \
+  nginx
+
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Open Speed Test"
+
+# Get the latest version of the speed test
+$STD git clone https://github.com/openspeedtest/Speed-Test.git /opt/openspeedtest/
+
+# Clean up permissions and link it to nginx's default web-root
+$STD chmod -R 755 /opt/openspeedtest/
+rm -rf /usr/share/nginx/html/
+mkdir -p /usr/share/nginx
+ln -s  /opt/openspeedtest/ /usr/share/nginx/html
+
+# Remove any existing default configuration
+$STD mv /etc/nginx/nginx.conf /etc/ngnix/nginx.conf.old
+
+# Get a preconfigured nginx configuration from the dev team
+$STD wget -q --content-disposition https://raw.githubusercontent.com/openspeedtest/Nginx-Configuration/refs/heads/main/nginx.conf 
+
+# Configure nginx pid config to use the /run directory
+sed -i 's|pid\s\+/tmp/nginx.pid;|pid        /run/nginx.pid;|' nginx.conf
+
+# Move the updated file to nginx config directory
+mv nginx.conf /etc/nginx/nginx.conf
+
+# Get a preconfigured copy of the nginx site configuration
+$STD wget -q --content-disposition https://raw.githubusercontent.com/openspeedtest/Nginx-Configuration/refs/heads/main/OpenSpeedTest-Server.conf 
+
+# Disable SSL from the nginx Config
+sed -i -e '/listen 3001 ssl reuseport;/s/^/#/' \
+       -e '/listen \[::\]:3001 ssl reuseport;/s/^/#/' \
+       -e '/ssl_certificate \/etc\/ssl\/nginx.crt;/s/^/#/' \
+       -e '/ssl_certificate_key \/etc\/ssl\/nginx.key;/s/^/#/' \
+       -e '/ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;/s/^/#/' \
+       -e '/ssl_ciphers "ALL";/s/^/#/' \
+       -e '/ssl_prefer_server_ciphers on;/s/^/#/' \
+       -e '/ssl_session_cache shared:SSL:100m;/s/^/#/' \
+       -e '/ssl_session_timeout 1d;/s/^/#/' \
+       -e '/ssl_session_tickets on;/s/^/#/' OpenSpeedTest-Server.conf 
+# Move the updated file to nginx config directory
+mv OpenSpeedTest-Server.conf /etc/nginx/conf.d/OpenSpeedTest-Server.conf 
+
+RELEASE=$(curl -s https://github.com/openspeedtest/Speed-Test/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+echo "${RELEASE}" >"/opt/${APP}_version.txt"
+
+msg_ok "Installed Open Speed Test"
+
+msg_info "Starting nginx Service"
+systemctl enable --now -q nginx
+systemctl restart -q nginx
+msg_ok "Service Started"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
