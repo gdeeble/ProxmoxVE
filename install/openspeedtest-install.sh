875a92fe8d0aa468fe20a76af64f9bacc82e6bd1
Gary Deeble <gdeeble@gmail.com>
2024-12-30 16:49:37 -0500

    Remove unecessary nginx file delete

--- install/openspeedtest-install.sh
+++ install/openspeedtest-install.sh
@@ -37,9 +37,6 @@ rm -rf /usr/share/nginx/html/
 mkdir -p /usr/share/nginx
 ln -s  /opt/openspeedtest/ /usr/share/nginx/html
 
-# Remove any existing default configuration - Force through /dev/null to prevent exit
-mv /etc/nginx/nginx.conf /etc/ngnix/nginx.conf.old 2> /dev/null
-
 # Get a preconfigured nginx configuration from the dev team
 $STD wget -q --content-disposition https://raw.githubusercontent.com/openspeedtest/Nginx-Configuration/refs/heads/main/nginx.conf 
 
